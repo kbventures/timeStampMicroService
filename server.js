1d603d5943790e902b41960e238cc384fcf91f08
kbventures <kmb_x@hotmail.com>
2020-09-20 18:14:15 +0300

    First working attemp. Needs to be tested.

--- server.js
+++ server.js
@@ -30,3 +30,52 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get("/api/timestamp/:date_string?", (req,res)=>{
+  var timeParam = req.params.date_string;
+  // console.log(timeParam.toString());
+  
+  if (timeParam) {
+    console.log("First");
+
+    var date = new Date(timeParam);
+    var testParam = date.toString();
+    console.log(testParam);
+    // Not empty but invalid date 
+    if(testParam == "Invalid Date"){
+      console.log("Invalid date test");
+      return res.json({error : "Invalid Date" });
+    }
+
+    //Not empty and valid date
+    console.log("Valid Date Test");
+    
+    var currentTime = date.getTime();
+    var currentUTC = date.toUTCString();
+    return res.json({unix : currentTime, utc : currentUTC });
+  }
+  
+    //empty string scenario
+    console.log("Empty string test");
+    var date = new Date();
+    var currentTime = date.getTime();
+    var currentUTC = date.toUTCString();
+    res.json({unix : currentTime, utc : currentUTC });
+});
+
+
+/* 3 situations: 2 where the timeParam is not empty 
+- 1 where its valid the other invalid \
+and a third situation where it is empty */ 
+
+
+/* The API endpoint is GET [project_url]/api/timestamp/:date_string?
+A date string is valid if can be successfully parsed by new Date(date_string).
+Note that the unix timestamp needs to be an integer (not a string) specifying milliseconds.
+In our test we will use date strings compliant with ISO-8601 (e.g. "2016-11-20") because this will ensure an UTC timestamp.
+If the date string is empty it should be equivalent to trigger new Date(), i.e. the service uses the current timestamp.
+If the date string is valid the api returns a JSON having the structure
+{"unix": <date.getTime()>, "utc" : <date.toUTCString()> }
+e.g. {"unix": 1479663089000 ,"utc": "Sun, 20 Nov 2016 17:31:29 GMT"}
+If the date string is invalid the api returns a JSON having the structure
+{"error" : "Invalid Date" }. */
